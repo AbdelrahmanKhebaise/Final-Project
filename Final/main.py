63bc8438c7d8f9d2a5eef1414d2961bf58b15bfa
AbdelrahmanKhebaise <abdelrahmankhebaisa@gmail.com>
2025-01-21 13:01:42 -0800

    Update main.py

--- Final/main.py
+++ Final/main.py
@@ -42,10 +42,11 @@ def preload_songs():
     global songs_directory
     songs_directory = os.getcwd()  # Use the current directory
     preloaded_songs = [
-        "Taylor Swift - 22.mp3",
+        "Taylor Swift - 22",
         "Taylor Swift - Cruel Summer (Official Audio).mp3",
         "Taylor Swift - Look What You Made Me Do.mp3",
         "Taylor Swift - Love Story.mp3",
+        "Taylor Swift - Blank Space",
         "Taylor Swift - Shake It Off.mp3",
         "Taylor Swift - Style.mp3",
         "Taylor Swift - We Are Never Ever Getting Back Together.mp3",
@@ -60,7 +61,7 @@ root = Tk()
 root.title("Music Player")
 
 # Main area for music player
-main_frame = Frame(root, bg="white")
+main_frame = Frame(root, bg="pink")
 main_frame.pack(fill=BOTH, expand=True)
 
 label = Label(main_frame, text="Music Player", font=("Helvetica", 20))
@@ -72,16 +73,13 @@ song_listbox.pack(pady=20)
 play_button = Button(main_frame, text="Play", width=10, height=2, font=("Helvetica", 12), command=play_music)
 play_button.pack(side=LEFT, padx=10, pady=20)
 
-pause_button = Button(main_frame, text="Pause", width=10, height=2, font=("Helvetica", 12), command=pause_music)
-pause_button.pack(side=LEFT, padx=10, pady=20)
-
 stop_button = Button(main_frame, text="Stop", width=10, height=2, font=("Helvetica", 12), command=stop_music)
 stop_button.pack(side=LEFT, padx=10, pady=20)
 
 resume_button = Button(main_frame, text="Resume", width=10, height=2, font=("Helvetica", 12), command=resume_music)
 resume_button.pack(side=LEFT, padx=10, pady=20)
 
-load_button = Button(main_frame, text="Load Songs", width=20, height=2, font=("Helvetica", 12), command=load_songs)
+load_button = Button(main_frame, text="Add Songs", width=20, height=2, font=("Helvetica", 12), command=load_songs)
 load_button.pack(pady=20)
 
 # Preload songs
